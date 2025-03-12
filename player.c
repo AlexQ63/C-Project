00209a5a9838e96aee8e13e2d96dab011f2a33ac
Alexandre QUELLEC <alec515@hotmail.fr>
2025-03-12 09:02:21 +0100

    last adjustement

--- player.c
+++ player.c
@@ -5,11 +5,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "game.h"
+
 char * defineNextPlay() {
   	char * answer = malloc (2 * sizeof(char));
     printf("Did you select the right piece? Press  no  for return or enter anything for continue your play: ");
 	scanf("%2s", answer);
-
+	uppercase(answer);
 	return answer;
 }
 
